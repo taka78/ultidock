aea12d0b835de662a05f8ba482e056524396b606
taka78 <takaturgut@gmail.com>
2025-02-11 13:09:12 +0300

    Fixing file structure.

---
+++ docking/config.py
@@ -0,0 +1,9 @@
+# config.py 
+# Paths for directories 
+LIGANDS_DIR = "/mnt/g/docking/LIGANDS_DIR"
+DOCKING_DIR = "/mnt/g/docking/DOCKING_DIR"
+ANALYSIS_DIR = "/mnt/g/docking/ANALYSIS_DIR"
+VINA_DIR = "/mnt/g/docking/VINA_DIR"
+VINA_GPU_DIR = "/mnt/g/docking/VINA_GPU_DIR"
+MACRO_MOL_DIR = "/mnt/g/docking/MACRO_MOL_DIR"
+GPU_TYPE = "AMD"
